b08321f297e4df6c9994fd6ac6691c7cd02e0d12
Andras Tilhof <andras.tilhof@commeto.be>
2020-09-12 11:16:22 +0200

    First version

---
+++ andras_hello/src/be/commeto/andras/HelloTest.java
@@ -0,0 +1,12 @@
+package be.commeto.andras;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		
+		System.out.println("Hello World"+ i);
+	}
+
+}
